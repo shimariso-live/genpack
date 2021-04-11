24a58e2460eade8047e044fb36e876463b4bc9f9
Tomoatsu Shimada <shimada@walbrix.com>
2021-04-11 10:18:47 +0900

    with-mysql script added

--- __main__.py
+++ __main__.py
@@ -148,6 +148,7 @@ def main(base, workdir, arch, sync, bash, artifact, outfile=None, profile=None):
     put_resource_file(gentoo_dir, initlib, "initlib.cpp")
     put_resource_file(gentoo_dir, initlib, "initlib.h")
     put_resource_file(gentoo_dir, util, "build-kernel.py", "usr/local/sbin/build-kernel", True)
+    put_resource_file(gentoo_dir, util, "with-mysql.py", "usr/local/sbin/with-mysql", True)
     put_resource_file(gentoo_dir, util, "install-system-image", "usr/sbin/install-system-image", True)
     put_resource_file(gentoo_dir, util, "expand-rw-layer", "usr/sbin/expand-rw-layer", True)
     put_resource_file(gentoo_dir, util, "do-with-lvm-snapshot", "usr/sbin/do-with-lvm-snapshot", True)
