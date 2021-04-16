59291edbe2bb3d956a5c044eb01925cf201ed994
Tomoatsu Shimada <shimada@walbrix.com>
2021-04-16 09:15:32 +0900

    add default boot command to grub.cfg

--- qemu.py
+++ qemu.py
@@ -23,6 +23,20 @@ class Tmpmount():
         subprocess.check_call(sudo(["umount", self.tempdir.name]))
         self.tempdir.cleanup()
 
+grub_cfg = """set BOOT_PARTITION=$root
+loopback loop /system.img
+set root=loop
+set prefix=($root)/boot/grub
+if [ -f /boot/grub/grub.cfg ]; then
+    normal
+else
+    probe -u $BOOT_PARTITION --set=BOOT_PARTITION_UUID
+    linux /boot/kernel boot_partition_uuid=$BOOT_PARTITION_UUID
+    initrd /boot/initramfs
+    boot
+fi
+"""
+
 def run(rootfs_file, disk_image, drm=False):
     with open(disk_image, "w") as f:
         f.truncate(4 * 1024 * 1024 * 1024)
@@ -34,14 +48,14 @@ def run(rootfs_file, disk_image, drm=False):
             grub_dir = os.path.join(mountpoint, "boot/grub")
             subprocess.check_call(sudo(["mkdir", "-p", grub_dir]))
             with Tee(os.path.join(grub_dir, "grub.cfg")) as f:
-                f.write("set BOOT_PARTITION=$root\nloopback loop /system.img\nset root=loop\nset prefix=($root)/boot/grub\nnormal".encode("utf-8"))
-            subprocess.check_call(sudo(["grub-install", "--target=i386-pc", "--boot-directory=%s" % os.path.join(mountpoint, "boot"), 
+                f.write(grub_cfg.encode("utf-8"))
+            subprocess.check_call(sudo(["grub-install", "--target=i386-pc", "--skip-fs-probe", "--boot-directory=%s" % os.path.join(mountpoint, "boot"), 
                 "--modules=normal echo linux probe sleep test ls cat configfile cpuid minicmd vbe gfxterm_background png multiboot multiboot2 lvm xfs btrfs keystatus", loop]))
             subprocess.check_call(sudo(["cp", rootfs_file, os.path.join(mountpoint, "system.img")]))
     
     qemu_cmdline = ["qemu-system-x86_64", "-enable-kvm", "-M", "q35", "-drive", "file=%s,format=raw,index=0,media=disk,if=virtio" % disk_image,
         "-rtc", "base=utc,clock=rt", "-m", "4096", "-no-shutdown"]
-    if drm: qemu_cmdline += ["-display", "gtk,gl=on", "-vga", "virtio"]
+    if drm: qemu_cmdline += ["-display", "gtk,gl=on", "-vga", "virtio", "-device", "virtio-mouse", "-device", "virtio-keyboard"]
     subprocess.check_call(qemu_cmdline)
 
 if __name__ == "__main__":
