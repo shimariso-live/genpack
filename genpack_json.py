4d028e47e6765ab30a32b151aa54f6faf511ee63
Tomoatsu Shimada <shimada@walbrix.com>
2024-08-26 05:55:05 +0900

    allow running without genpack.json

--- genpack_json.py
+++ genpack_json.py
@@ -1,10 +1,12 @@
-import json,logging
+import json,logging,os
 
 _genpack_json = None
 
 def load():
     global _genpack_json
     if _genpack_json is None:
+        if not os.path.isfile("./genpack.json"): return {}
+        #else
         _genpack_json = json.load(open("./genpack.json"))
         logging.debug("genpack.json loaded")
     return _genpack_json
